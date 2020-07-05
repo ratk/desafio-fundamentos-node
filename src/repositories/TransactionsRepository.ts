e899d4504fbdd1b95fb831f6aeb7624514639b0c
danielratk pc <danielratk@gmail.com>
2020-07-04 22:38:23 -0300

    Desafio 05: Primeiro projeto Node.js

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,18 @@
+import { uuid } from 'uuidv4';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +23,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    this.transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        balance.income += transaction.value;
+      }
+
+      if (transaction.type === 'outcome') {
+        balance.outcome += transaction.value;
+      }
+
+      balance.total = balance.income - balance.outcome;
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
